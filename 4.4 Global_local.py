673dd854fa5ba08f188ffa24ffb276fc185d5df2
HeyArtem <artem_white@mail.ru>
2020-05-27 13:57:30 +0300

    4.4 Global_local_variable_nonlocal

--- 4.4 Global_local.py
+++ 4.4 Global_local.py
@@ -25,8 +25,33 @@ print()
 print('     Служебное слово GLOBAL. Для спец. манипупуляц. с переменными      ')
 
 def func_exemple_1(local_var_1, local_var_2):
-    global global_var = 20
+    global global_var
+    global_var = 20
     print(local_var_1, local_var_2, global_var, id(global_var))
 
 func_exemple_1(11, 12)
 print(global_var, id(global_var))
+
+print()
+
+# Так же есть служебное слово    - nonlocal - . C помощью nonlocal можно делать переопределение внутренней области.
+print('    Так же есть служебное слово    - nonlocal - . C помощью nonlocal можно делать переопределение внутренней области.  ')
+
+def counter():
+    num = 0
+    def plus_one():
+        nonlocal num
+        num+=1
+        return num
+    return plus_one
+
+count = counter()
+print(count)
+
+print('*')
+print(count())
+# Я не понял!! Что то написали..работает.. а не должно.. еще что то. Более подробный материал будет прикреплен к лекции
+
+
+print('* * ')
+print(count())
