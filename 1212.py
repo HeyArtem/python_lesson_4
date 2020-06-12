dc296f6c62a2c129c9557e5fda4f6e6f47a7a9d2
HeyArtem <artem_white@mail.ru>
2020-06-12 20:59:13 +0300

    домашка с промежуточными шагами, немного replace

--- 1212.py
+++ 1212.py
@@ -98,6 +98,196 @@ def rare_letter(lst):
 print(rare_letter(f_list))
 
 
+print()
+print('  -    -    ')
+print()
+
+#  4. В файле с логами найти дату самого позднего лога (по метке времени):
+print('      4. В файле с логами найти дату самого позднего лога (по метке времени): ')
+print()
+
+
+
+
+
+
+with open('log', 'r') as f:
+    from datetime import datetime
+    lines = f.readlines()  # Сделали список из лога
+    print('печатаю перемен lines: ', lines) #ПРО
+    all_date_times = list(map(lambda i: i.split(',')[0], lines)) # напечатали только дату
+    print('печатаю all_date_times: ', all_date_times)
+    all_date_times.sort(key=lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S')) # отсортирована дата от ранней к последней
+    print('All date times which are sorted Даты отсортированы:\n', all_date_times)
+    print('The latest log line is: ', all_date_times[len(all_date_times)-1])
+
+
+
+
+
+
+print(' - * - * - ')
+print(' - * - * - ')
+
+f = open(r"C:\Users\artem\Documents\Артем\PycharmProjects\python_lesson_4\log", "r")
+text = f.read()
+print(text)
+print(type(text))
+f.close()
+
+
+
+# from datetime import datetime
+
+take_date =list(map(lambda i: i.split(' ')[0], text)) #перебираем каждого участника, тут неважно что будет стоять в качестве разделителя
+# не понимаю для чего [0], возможно это на сколько часте разбивать. Вывод првратили строку в список состоящий из каждого элемента
+print(take_date)
+print('тип перемен take_date: ', type(take_date))
+
+print('Хочу методом replace заменить "\\n" на " " : ')
+split_noN = text.replace("\n", " ")
+print('шаг 1 удалили заменили переносы строки на " , ": ', split_noN)
+print(type(split_noN))
+
+
+s=' '.join(take_date) # склеили по точке
+print(s)
+
+print('Хочу методом replace заменить " " на " " : ')
+split_noN_br = text.replace(" ", "")
+print('шаг 1 удалили заменили переносы строки на " , ": ', split_noN_br)
+print(type(split_noN_br))
+
+#take_date.sort(key=lambda date: datetime.strptime(date, '%y-%m-%d %H:%M:%S'), reverse=True)   # ТАК И НЕ ЗАРАБОТАЛО ПОЧЕМУ???
+
+# ГОВНО!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+# sort -сортирет по возрастанию или убыванию (sorted-СОЗДАЕТ НОВУЮ отсор-ю после-ть), если указать «reverse = true»,
+# список будет отсортирован в порядке убывания. ВИД  -> sort[List_name.sort(key = …, reverse = ...)]
+# key -ключ по которому сортируем, в данном случае это функция(lambda)
+# здесь lambda - datetime.strptime - преобразует строку в datetime
+#print(take_date2)
+# all_date_times.sort(key=lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), reverse=True)
+
+
+#print()
+
+print('Хочу методом replace заменить \\n на "." : ')
+split_noN = text.replace("\n", ". ")
+print('шаг 1 удалили заменили переносы строки на " , ": ', split_noN)
+print(type(split_noN))
+
+
+
+
+
+
+
+print()
+
+print()
+print('        сортирую по запятой  , ')
+
+split1 = text.split(",")
+print(type(split1))
+print("split1 :",  split1)
+
+
+
+print()
+
+# # делаю из split1 строку, т.к. split не хочет работать с List странно вооюще
+# print('делаю из split1 строку, т.к. split не хочет работать с List странно вооюще')
+# sp1 = str(split1)
+
+print()
+# print('сортирую по запятой')  # Это добавляет [" [блаБла]"]  только усложняет
+# sp2=sp1.split(",")
+# print('это sp2после сортировки по запятым', sp2)
+# print(type(sp2))
+
+
+
+
+# split2 = sp1.split(",") # разделил по запятой
+# print(type(split2))
+# print(split2)
+
+print()
+
+print('!!! !!!')
+# NEW Метод  -  replace      -> str.replace(old, new, count)<- можно без count
+print('      NEW Метод  -  replace      -> str.replace(old, new, count)<- можно без count')
+print('Hello'.replace('l', '-Ж-'))
+
+print()
+
+po = ('Зина, Артур, Илья, Семен, Просковья,Маргарита, Альберт')
+po1 = po.replace('а', ' - БАЦ - ')
+print('В перемен-ой заменили a на БАЦ: работает, но весь текст должен быть в кавычках', po1)
+
+
+
+
+
+
+print()
+# print('печатаю первый и третий элемент')
+# print(split[0]) # печатаем конкретный элемент
+# print(split[2])
+
+
+
+
+
+
+print(' - * - ')
+
+# sps_list = text.split('\n')
+# print(type(sps_list), sps_list)
+
+# strX = str(sps_list) # пробую перевести в строку
+# print(type(strX), strX)
+#
+
+print(' - * * - ')
+
+
+# split2 = strX.split(",") # разделил по запятой
+# print(type(split2))
+# print(split2)
+
+
+
+
+
+
+
+
+
+
+
+
+# lines = f.readlines()
+# print(lines)
+# all_date_times = list(map(lambda i: i.split(',')[0], lines))
+# print(all_date_times)
+# all_date_times.sort(key=lambda date: datetime.strptime(date, '%y-%m-%d %H-%M-%S'), reverse=True)
+# print('All date times which are sorted:\n', all_date_times)
+# print('The latest log line is: ', all_date_times[len(all_date_times)-1])
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
